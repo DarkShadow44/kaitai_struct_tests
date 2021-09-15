2604b05a10f6f63b5442346e120fb033fceecfa3
Fabian Maurer <dark.shadow4@web.de>
2021-09-16 01:39:19 +0200

    Add operatos for bytes

--- spec/c/prereq/operators.h
+++ spec/c/prereq/operators.h
@@ -1,4 +1,6 @@
+#include <kaitaistruct.h>
 #include <iostream>
+#include <sstream>
 
 inline bool operator==(const ks_string s1, const char* s2)
 {
@@ -8,4 +10,59 @@ inline bool operator==(const ks_string s1, const char* s2)
 inline std::ostream& operator<<(std::ostream& out, const ks_string s)
 {
     return out << s.data;
+}
+
+inline bool operator==(const ks_bytes b1, const ks_bytes b2)
+{
+    uint8_t* data1;
+    uint8_t* data2;
+    uint64_t length1, length2;
+
+    ks_bytes_get_length(&b1, &length1);
+    ks_bytes_get_length(&b2, &length2);
+
+    if (length1 != length2)
+        return false;
+
+    data1 = (uint8_t*)malloc(length1);
+    data2 = (uint8_t*)malloc(length2);
+    ks_bytes_get_data(&b1, data1);
+    ks_bytes_get_data(&b2, data2);
+
+    for (int i = 0; i < length1; i++)
+    {
+        if (data1[i] != data2[i])
+        {
+            free(data1);
+            free(data2);
+            return false;
+        }
+    }
+
+    free(data1);
+    free(data2);
+    return true;
+}
+
+inline std::ostream& operator<<(std::ostream& out, const ks_bytes b)
+{
+    uint8_t* data;
+    uint64_t length;
+    std::stringstream ss;
+
+    ks_bytes_get_length(&b, &length);
+    data = (uint8_t*)malloc(length);
+    ks_bytes_get_data(&b, data);
+
+    bool first = true;
+    for (int i = 0; i < length; i++)
+    {
+        if (!first)
+            ss << ", ";
+        ss << data[i];
+        first = false;
+    }
+    free(data);
+
+    return out << ss.str();
 }
